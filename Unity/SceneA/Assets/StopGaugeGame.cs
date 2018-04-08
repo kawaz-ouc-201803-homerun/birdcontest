ace0b5d7a6c481c94ecb0266024c45a7083b5beb
Takada Akito <>
2018-04-08 18:53:11 +0900

    no message

---
+++ Unity/SceneA/Assets/StopGaugeGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopGaugeGame : MonoBehaviour {
+	private int power = 0;
+	bool chack = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!chack){
+			power++;
+			if (Input.GetKeyDown(KeyCode.K) && power<=100) {
+				chack = true;
+				Debug.Log (power);
+			}	
+		}
+	}
+}
