2b64f880ba6c546c56c45c72823f59460ff154aa
ootomo <a@gmail.com>
2018-04-15 18:32:53 +0900

    クリックにより説明表示，スコア変化

--- Unity/SceneC/Assets/Scripts/ButtonController.cs
+++ Unity/SceneC/Assets/Scripts/ButtonController.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ButtonController : MonoBehaviour {
+
+	static public int score = 0;	//スコア
 	public GameObject[] setsumei;	//説明図
 	public GameObject YesNoButton;	//２回目押すボタン
+	public GameObject matometekesuObject;	//消す
 
 	// Use this for initialization
 	void Start () {
@@ -16,22 +19,29 @@ public class ButtonController : MonoBehaviour {
 	void Update () {
 	}
 
-	public void ButtonOnClick (string str){			
+	public void ButtonOnClick (string str){
 		switch (str) {
 		//ボタン１回目
-		case "Bomb":
+		case "Bomb":	//爆弾を押した
+			score += 1;
 			StartCoroutine(SetsumeiHyouji(0));			
 			break;
-		case "Coin":
+		case "Coin":	//賄賂を押した
+			score += 2;
 			StartCoroutine(SetsumeiHyouji(1));
+			break;
+		case "Kiai":	//気合いを押した
+			score += 0;
+			StartCoroutine(SetsumeiHyouji(2));
 			break;
 		//ボタン２回目
 		case "Yes":
+			StartCoroutine(SetsumeiHihyouji(matometekesuObject));
 			break;
 		case "No":
-			int i;
-			for(i = 0; i <= setsumei.Length; i++){
-				StartCoroutine(SetsumeiHihyouji(i));
+			score = 0;
+			foreach(GameObject obj in setsumei){
+				StartCoroutine(SetsumeiHihyouji(obj));
 			}
 			break;
 		default:
@@ -39,8 +49,9 @@ public class ButtonController : MonoBehaviour {
 		}
 	}
 
-	IEnumerator SetsumeiHyouji(int i){		//説明図をフェードイン
+	IEnumerator SetsumeiHyouji(int i){		//説明をフェードイン
 		yield return new WaitForSeconds(0.1f);
+		setsumei[i].SetActive(true);
 		for (float f = 0; f < 1; f += 0.1f) {
         	Color c = setsumei[i].GetComponent<Image>().color;
         	c.a = f;
@@ -48,21 +59,23 @@ public class ButtonController : MonoBehaviour {
 			yield return new WaitForEndOfFrame();
 		}
 		setsumei[i].GetComponent<Image>().color = new Color(1,1,1,1);
+		yield return new WaitForSeconds(0.2f);
 		YesNoButton.SetActive(true);
 	}
-	IEnumerator SetsumeiHihyouji(int i){		//説明図をフェードアウト
+	IEnumerator SetsumeiHihyouji(GameObject obj){		//説明をフェードアウト（どうして上と引数の型が違うの〜）
 		yield return new WaitForSeconds(0.1f);
 		YesNoButton.SetActive(false);
 		for (float f = 1; f > 0; f -= 0.1f) {
-        	Color c = setsumei[i].GetComponent<Image>().color;
+        	Color c = obj.GetComponent<Image>().color;
         	c.a = f;
-        	setsumei[i].GetComponent<Image>().color = c;
+        	obj.GetComponent<Image>().color = c;
 			yield return new WaitForEndOfFrame();
 		}
-		setsumei[i].GetComponent<Image>().color = new Color(1,1,1,0);
+		obj.GetComponent<Image>().color = new Color(1,1,1,0);
+		obj.SetActive(false);
 	}
 
-	float HokanKansu(float f){
+	float HokanKansu(float f){	//補間函数（使ってない）
 		return -2*Mathf.Pow(f,3)+3*Mathf.Pow(f,2);
 	}
 }
