11bcdb11ac2692e3c14bf46aa36a7445990f592f
TEMPPC <temp@gmail.com>
2018-04-29 17:22:23 +0900

    連打とスライダー機能の実装

---
+++ Unity/ControllerB/Assets/Scripts/MainB.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainB : MonoBehaviour {
+	
+	public bool flag;
+
+	public int count;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("MainB");	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			this.flag = true;
+		}
+		if (Input.GetMouseButtonUp (0) && this.flag==true) {
+			this.count++;
+			this.flag = false;
+			GameObject.Find ("Slider").GetComponent<UnityEngine.UI.Slider> ().value = this.count;
+
+		}
+	}
+}
---
+++ Unity/ControllerB/Assets/Scripts/StartButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour {
+
+	public GameObject First;
+	public GameObject Seccond;
+	public GameObject End;
+
+	// すたーとぼたんをおしたときのがめんせんい
+	public void OnClick () {
+		this.First.SetActive(false);
+		this.Seccond.SetActive (true);
+	}
+
+}
