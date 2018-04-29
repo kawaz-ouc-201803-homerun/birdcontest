79491907cf28090bb6aec29caa6eb16ff7ff8c83
TomokiPC-Parent\tomokis <ts32worker@gmail.com>
2018-04-30 02:29:23 +0900

    デバッグ用に、EnterキーとEscキーでフェーズを前後切り替えられるようにしました

--- Unity/GameMaster/Assets/Scripts/Phase/PhaseManager.cs
+++ Unity/GameMaster/Assets/Scripts/Phase/PhaseManager.cs
@@ -14,9 +14,44 @@ using UnityEngine.SceneManagement;
 public class PhaseManager : MonoBehaviour {
 
 	/// <summary>
+	/// フェーズごとのUI親オブジェクト
+	/// </summary>
+	public GameObject[] PhaseUIs;
+
+	/// <summary>
 	/// 現在のフェーズ
 	/// </summary>
 	private PhaseBase phase;
+
+	/// <summary>
+	/// フェーズのインデックス
+	/// </summary>
+	private int phaseIndex {
+		get {
+			return this._phaseIndex;
+		}
+		set {
+			if(value < 0 || this.PhaseUIs.Length <= value) {
+				return;
+			}
+
+			// 対応するUIブロックに表示を切り替える
+			this.PhaseUIs[this._phaseIndex].SetActive(false);
+			this._phaseIndex = value;
+			this.PhaseUIs[value].SetActive(true);
+		}
+	}
+	private int _phaseIndex;
+
+	/// <summary>
+	/// 前のフェーズに戻るキーのフレームカウンター
+	/// </summary>
+	private int previousKeyCounter;
+
+	/// <summary>
+	/// 次のフェーズに進むキーのフレームカウンター
+	/// </summary>
+	private int nextKeyCounter;
 
 	/// <summary>
 	/// 初期設定
@@ -25,13 +60,80 @@ public class PhaseManager : MonoBehaviour {
 		// フェーズをアイドル状態にセット
 		this.phase = new PhaseIdle();
 
-		// TODO: 各種変数を初期化
+		// 各種変数を初期化
+		this.phaseIndex = 0;
+		this.previousKeyCounter = 0;
+		this.nextKeyCounter = 0;
 	}
 
 	/// <summary>
 	/// 毎フレームの処理
 	/// </summary>
 	void Update() {
+#if UNITY_EDITOR
+		// デバッグ時のみ有効な処理
+
+		// 戻るボタン
+		if(Input.GetKeyDown(KeyCode.Escape) == true) {
+			this.previousKeyCounter++;
+			if(this.previousKeyCounter == 1) {
+				// 前のフェーズへ戻る
+				switch(this.phase.GetType().Name) {
+					case "PhaseIdle":
+						// これ以上戻れない
+						break;
+
+					case "PhaseControllers":
+						this.phase = new PhaseIdle();
+						this.phaseIndex--;
+						break;
+
+					case "PhaseFlight":
+						this.phase = new PhaseControllers();
+						this.phaseIndex--;
+						break;
+
+					case "PhaseResult":
+						this.phase = new PhaseFlight(null);
+						this.phaseIndex--;
+						break;
+				}
+			}
+		} else {
+			this.previousKeyCounter = 0;
+		}
+
+		// 進むボタン
+		if(Input.GetKeyDown(KeyCode.Return) == true) {
+			this.nextKeyCounter++;
+			if(this.nextKeyCounter == 1) {
+				// 前のフェーズへ戻る
+				switch(this.phase.GetType().Name) {
+					case "PhaseIdle":
+						this.phase = new PhaseControllers();
+						this.phaseIndex++;
+						break;
+
+					case "PhaseControllers":
+						this.phase = new PhaseFlight(null);
+						this.phaseIndex++;
+						break;
+
+					case "PhaseFlight":
+						this.phase = new PhaseResult(null);
+						this.phaseIndex++;
+						break;
+
+					case "PhaseResult":
+						// これ以上進めない
+						break;
+				}
+			}
+		} else {
+			this.nextKeyCounter = 0;
+		}
+#endif
+
 		// TODO: ゲームマスター共通の処理
 
 		this.phase.Update();
@@ -44,6 +146,7 @@ public class PhaseManager : MonoBehaviour {
 	/// <param name="e">イベント引数</param>
 	private void changePhaseEventHandler(object sender, PhaseBase.NextPhaseEventArgs e) {
 		this.phase = e.NextPhase;
+		this.phaseIndex++;
 
 		// 次のフェーズがないとき、ゲームの１サイクルが終了したとみなして現在のシーンをリロードする
 		if(this.phase == null) {
